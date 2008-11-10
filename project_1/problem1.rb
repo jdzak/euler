b71a1b0905ebeb4cb01fcfc33ad34aa0ead4ead1
John Dzak <johndzak@poop-2.gateway.2wire.net>
2008-11-09 22:33:38 -0600

    Used union operator to combine arrays and remove dups.

--- project_1/problem1.rb
+++ project_1/problem1.rb
@@ -4,8 +4,7 @@ require 'array'
 multiples_of_3 = 3.multiples(1..999)
 multiples_of_5 =  5.multiples(1..999)
 
-multiples = multiples_of_3 + multiples_of_5
-unique_multiples = multiples.uniq
+unique_multiples = multiples_of_3 | multiples_of_5
 
 sum = unique_multiples.sum
 
