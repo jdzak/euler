aa94698a28eaa628bcb2cbcf776afcad244a9da2
John Dzak <john.dzak@gmail.com>
2009-08-04 09:26:08 -0500

    Make sure left and right quantities are not the same.

--- project_4/project_4.rb
+++ project_4/project_4.rb
@@ -5,7 +5,7 @@ palindromes = []
 999.downto(500) do |left|
   999.downto(500) do |right|
     product = left * right
-    palindromes << product if product.to_s.palindrome?
+    palindromes << product if product.to_s.palindrome? && left != right
   end
 end
 
